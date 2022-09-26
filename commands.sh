0c5844ae0aca89d6275fd0ae5b8e4f89b47728be
Kirill <kirilledition@protonmail.com>
2022-09-26 18:18:13 +0000

    fix build-arg

--- commands.sh
+++ commands.sh
@@ -1,3 +1,3 @@
 docker build -f x86.Dockerfile --build-arg MAKE_JOBS=32 -t build_gcta .
-docker build -f arm.Dockerfile --build-arg MAKE_JOBS=32 CPU_ARCH=NEOVERSEN1 -t build_gcta .
+docker build -f arm.Dockerfile --build-arg MAKE_JOBS=32 --build-arg CPU_ARCH=NEOVERSEN1 -t build_gcta .
 docker run -it --rm -v "$(pwd)"/result:/result build_gcta cp /home/gcta/build/gcta64 /result/gcta64
